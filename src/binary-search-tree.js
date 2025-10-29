84fbcd9646665f95e9cc3199df4d5133b91276db
stylerjulian <andreymatveyev2000@gmail.com>
2025-10-29 19:13:49 +0300

    binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,44 +1,136 @@
 import { NotImplementedError } from "../extensions/index.js";
 
-// import { Node } from '../extensions/list-tree.js';
+import { Node } from '../extensions/list-tree.js';
 
 /**
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
 export default class BinarySearchTree {
+  constructor() {
+    this._root = null;
+  }
+
   root() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+    
+    if (this._root === null) {
+      this._root = newNode;
+      return;
+    }
+    
+    this._addNode(this._root, newNode);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  _addNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this._addNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this._addNode(node.right, newNode);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  has(data) {
+    return this.find(data) !== null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  find(data) {
+    return this._findNode(this._root, data);
+  }
+
+  _findNode(node, data) {
+    if (node === null) {
+      return null;
+    }
+    
+    if (data < node.data) {
+      return this._findNode(node.left, data);
+    } else if (data > node.data) {
+      return this._findNode(node.right, data);
+    } else {
+      return node;
+    }
+  }
+
+  remove(data) {
+    this._root = this._removeNode(this._root, data);
+  }
+
+  _removeNode(node, data) {
+    if (node === null) {
+      return null;
+    }
+    
+    if (data < node.data) {
+      node.left = this._removeNode(node.left, data);
+      return node;
+    } else if (data > node.data) {
+      node.right = this._removeNode(node.right, data);
+      return node;
+    } else {
+      // Node to be deleted found
+      
+      // Case 1: Node with no children
+      if (node.left === null && node.right === null) {
+        return null;
+      }
+      
+      // Case 2: Node with only one child
+      if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      }
+      
+      // Case 3: Node with two children
+      // Find the smallest node in the right subtree (inorder successor)
+      const minRight = this._findMinNode(node.right);
+      node.data = minRight.data;
+      node.right = this._removeNode(node.right, minRight.data);
+      return node;
+    }
+  }
+
+  _findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    }
+    return this._findMinNode(node.left);
   }
 
   min() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    if (this._root === null) {
+      return null;
+    }
+    
+    let current = this._root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
   }
 
   max() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    if (this._root === null) {
+      return null;
+    }
+    
+    let current = this._root;
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
   }
-};
+}
